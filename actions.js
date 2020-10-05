7a0abd145e0caff1875fc19d4b3c150d7b54c3d4
naina <nanu02d8@gmail.com>
2020-10-05 23:40:06 +0530

    added script to check correct square

--- actions.js
+++ actions.js
@@ -1,22 +1,34 @@
 let colors = [
-    "rgb(255,0,0)",
-    "rgb(255,255,0)",
-    "rgb(0,255,255)",
-    "rgb(255,0,255)",
-    "rgb(0,255,0)",
-    "rgb(0,0,255)"
-]
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 0, 255)"
+];
 
-let squares = document.getElementsByClassName("buttons");
+let squares = document.querySelectorAll(".buttons");
 let pickedColor = colors[3];
-let colorDisplay = document.getElementById("rgbVal");
+let colorDisplay = document.querySelector('#rgbVal');
+let displayMessage = document.querySelector('#message');
+
+colorDisplay.textContent = pickedColor;
 for( let i = 0 ; i<squares.length; i++){
     // set background color
     squares[i].style.backgroundColor = colors[i];
     // add event listeners 
-    squares[i].addEventListener('click',() => {
-        alert("hey!");
-    })
+    squares[i].addEventListener("click",function(){
+        // grab the bg color of clicked square
+        let clickedSquare = this.style.backgroundColor;
+        // compare color to the pickedcolor
+        if(clickedSquare === pickedColor){
+            alert('right!');
+            displayMessage.textContent = "Correct";
+        }
+        else{
+            this.style.backgroundColor = "rgb(5, 4, 4)";
+            displayMessage.textContent = "Incorrect!";
+        }
+    });
 }
 
-colorDisplay.textContent = pickedColor;
